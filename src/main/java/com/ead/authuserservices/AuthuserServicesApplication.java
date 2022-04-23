d8d11f59b505caa25deda9669bf2d62341e2aa6a
Artur Oscar <Arturoscar3@gmail.com>
2022-04-23 16:34:06 -0300

    First commit

---
+++ src/main/java/com/ead/authuserservices/AuthuserServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ead.authuserservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuserServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthuserServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ead/authuserservices/AuthuserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.authuserservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
