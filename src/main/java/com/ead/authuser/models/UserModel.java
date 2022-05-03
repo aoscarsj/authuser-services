18eee5e81844312990856d51cedd9f4e571821f7
Artur Oscar <Arturoscar3@gmail.com>
2022-05-02 22:07:37 -0300

    Adding userModel

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -6,14 +6,20 @@ import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
 @Table(name = "TB_USERS")
@@ -27,7 +33,7 @@ public class UserModel implements Serializable {
     private String username;
     @Column(nullable = false, unique = true, length = 50)
     private String email;
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false)
     @JsonIgnore
     private String password;
     @Column(nullable = false, length = 250)
@@ -35,8 +41,8 @@ public class UserModel implements Serializable {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
-    @Enumerated
-    @Column(name = "user_type")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserType userType;
     @Column(length = 20)
     private String phoneNumber;
@@ -50,5 +56,4 @@ public class UserModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime lastUpdateDate;
-
 }
